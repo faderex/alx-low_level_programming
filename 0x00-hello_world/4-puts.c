1c023c7de368ee1845a6b78231608af811e0f764
Joseph Larbi <josephlarbi264@gmail.com>
2021-06-18 09:01:40 -0700

    heyoo

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include  <stdio.h>
+/**
+ * main - Print a string
+ * Description: use printif
+ * Return: 0
+ */
+int main(void)
+{
+  printif("%s", "Programming is like bulding a multilingual puzzle);
+return (0);
+}
